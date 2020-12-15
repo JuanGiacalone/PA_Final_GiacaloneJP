cbabfaae6ba367d8082b7f580f1340999bc06122
Juan Pablo Giacalone <juanpablogiacalone8@gmail.com>
2020-12-14 21:10:38 -0300

    init VtG

---
+++ vtg/target/debug/deps/vtg-08e76c4f8ec582ce.d
@@ -0,0 +1,5 @@
+/usr/local/code/progra_Avanzada/PA_Final_GiacaloneJP/vtg/target/debug/deps/vtg-08e76c4f8ec582ce: src/main.rs
+
+/usr/local/code/progra_Avanzada/PA_Final_GiacaloneJP/vtg/target/debug/deps/vtg-08e76c4f8ec582ce.d: src/main.rs
+
+src/main.rs:
---
+++ vtg/target/debug/vtg.d
@@ -0,0 +1,1 @@
+/usr/local/code/progra_Avanzada/PA_Final_GiacaloneJP/vtg/target/debug/vtg: /usr/local/code/progra_Avanzada/PA_Final_GiacaloneJP/vtg/src/main.rs
